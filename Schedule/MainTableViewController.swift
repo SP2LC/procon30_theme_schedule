cef6f25042093483954ae29074b5a676b391a77e
kokimaruo <s16545@salesio-sp.ac.jp>
2019-06-19 18:39:47 +0900

    add TableView
    基礎となるテーブルビューを追加しました。

--- Schedule/MainTableViewController.swift
+++ Schedule/MainTableViewController.swift
@@ -17,7 +17,7 @@ class MainTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func didReceiveMemoryWarning() {
@@ -56,17 +56,18 @@ class MainTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
--- Schedule/ViewController.swift
+++ Schedule/ViewController.swift
@@ -8,13 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    //配列fruitsを設定
+    var fruits = ["apple", "orange", "melon", "banana", "pineapple"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 編集ボタンを左上に配置
+        navigationItem.rightBarButtonItem = editButtonItem
+        
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
     
+    //cellの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
     
+    //tableViewの表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell
+    }
     
+    //cellの追加
+    @IBAction func tapAdd(sender: AnyObject) {
+        // myItemsに追加.
+        fruits.append("タイトルNEW")
+        // TableViewを再読み込み.
+        TableView.reloadData()
+    }
+
+    //editボタンが押された時の処理
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: true)
+        TableView.isEditing = editing
+    }
+    /*
+    //cellを削除可能に
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+    //cellの削除
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCell.EditingStyle.delete) {
+            fruits.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    /*
+    //cellを並び替え可能に
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //sourceIndexPath にデータの元の位置、destinationIndexPath に移動先の位置
+        //CellValueを取得
+        let targetTitle = fruits[sourceIndexPath.row]
+        //元の位置のデータを配列から削除
+        fruits.remove(at:sourceIndexPath.row)
+        //移動先の位置にデータを配列に挿入
+        fruits.insert(targetTitle, at: destinationIndexPath.row)
+        
+    }
+
 }
